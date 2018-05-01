ca4c03411064097b475c659ab54d55f8b1230784
Mario Hidalgo Garcia <MarioHidalgoTi@gmail.com>
2018-05-01 22:10:47 +0200

    Shows LOG information and a warning to the registration of each request on your "Hello World" page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace G3\FrameworkPractice\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -11,15 +12,20 @@ final class MainController extends Controller
 {
     private $environment;
     private $environmentName;
+    private $logger;
 
-    public function __construct(string $environment, string $environmentName)
+    public function __construct(string $environment, string $environmentName, LoggerInterface $logger)
     {
         $this->environment     = $environment;
         $this->environmentName = $environmentName;
+        $this->logger          = $logger;
     }
 
     public function showHelloEnv()
     {
+        $this->logger->info('Info to the log on each request to Hello Word');
+        $this->logger->warning('Waring to the log on each request to Hello Word');
+
         return new Response(
             '<html><body>Hello ' . $this->environment . ' World</body></html>'
         );
@@ -27,6 +33,10 @@ final class MainController extends Controller
 
     public function showHelloEnvName()
     {
+
+        $this->logger->info('Info to the log on each request to Hello Word');
+        $this->logger->warning('Waring to the log on each request to Hello Word');
+
         return new Response(
             '<html><body>Hello ' . $this->environmentName . ' World</body></html>'
         );
