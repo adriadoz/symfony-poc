23085b4f511e941445acfee22c9f52cb33f68ada
Mario Hidalgo Garcia <MarioHidalgoTi@gmail.com>
2018-05-04 20:04:11 +0200

    Select the environment to print specifying it as a command argument

--- src/Infrastructure/Log/LogSummaryConsole.php
+++ src/Infrastructure/Log/LogSummaryConsole.php
@@ -5,32 +5,44 @@ declare(strict_types = 1);
 namespace G3\FrameworkPractice\Infrastructure\Log;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
 
 final class LogSummaryConsole extends Command
 {
-    private $environment;
-
-    public function __construct(string $environment)
-    {
-        parent::__construct();
-        $this->environment = $environment;
-    }
-
     protected function configure(): void
     {
         $this
             ->setName('log:summary')
-            ->setDescription('Print summary of last 15 day log');
+            ->setDescription('Print summary of last 15 day log')
+            ->addArgument(
+                'environment',
+                InputArgument::OPTIONAL,
+                '[string] Enter a environment to show. Example: "dev"'
+            );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
-        $logGetters = new LogSummaryGetter($this->environment);
-        $logSummary = $logGetters->__invoke();
+        $environment = $this->getEnvironment($input, $output);
+        $logGetters  = new LogSummaryGetter($environment);
+        $logSummary  = $logGetters->__invoke();
 
         $this->print($logSummary->__invoke(), $output);
+    }
+
+    private function getEnvironment(InputInterface $input, OutputInterface $output): string
+    {
+        $helper             = $this->getHelper('question');
+        $enteredEnvironment = $input->getArgument('environment');
+        if (empty($enteredEnvironment)) {
+            $question           = new Question('Please enter a environment to show: ', 'dev');
+            $enteredEnvironment = $helper->ask($input, $output, $question);
+        }
+
+        return $enteredEnvironment;
     }
 
     private function print(array $summaryLog, OutputInterface $output): void
@@ -38,6 +50,5 @@ final class LogSummaryConsole extends Command
         foreach ($summaryLog as $key => $value) {
             $output->writeln($key . ": " . $value);
         }
-
     }
 }
