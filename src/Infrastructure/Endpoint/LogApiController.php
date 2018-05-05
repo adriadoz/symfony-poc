326155885c395a771ca9d17c687c29da53f486ee
Mario Hidalgo Garcia <MarioHidalgoTi@gmail.com>
2018-05-05 16:52:25 +0200

    Implement the route using Annotations (composer require annotations)

--- src/Infrastructure/Endpoint/LogApiController.php
+++ src/Infrastructure/Endpoint/LogApiController.php
@@ -6,12 +6,19 @@ namespace G3\FrameworkPractice\Infrastructure\Endpoint;
 
 use G3\FrameworkPractice\Application\Endpoint\LogApiBuilder;
 use G3\FrameworkPractice\Infrastructure\Log\LogSummaryGetter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+
 
 final class LogApiController
 {
     const PATH = '../var/log/';
 
+    /**
+     * @Route("/logs", name="log_api")
+     * @Method({"GET"})
+     */
     public function __invoke(): Response
     {
         $logSummary    = new LogSummaryGetter(self::PATH, $_SERVER['APP_ENV']);
