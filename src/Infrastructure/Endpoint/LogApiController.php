2e660ad6a4eeeb691c0a53ebe284167f1ecfe2a9
Adria Velardos Palomar <adria.velardos@habitaclia.com>
2018-05-06 23:32:47 +0200

    Add controller that shows Twig template

--- src/Infrastructure/Endpoint/LogApiController.php
+++ src/Infrastructure/Endpoint/LogApiController.php
@@ -11,19 +11,29 @@ use G3\FrameworkPractice\Infrastructure\Log\LogSummaryGetter;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Twig_Environment;
+use Twig_Loader_Filesystem;
 
 final class LogApiController extends Controller
 {
     private const PATH = '../var/log/';
     private $repository;
+    private $environment;
+    private $loader;
+    private $twig;
 
     public function __construct(LogRepositoryInterface $repository)
     {
         $this->repository = $repository;
+        $this->loader = new Twig_Loader_Filesystem('../templates');
+        $this->twig = new Twig_Environment($this->loader, array(
+            'cache' => '../var/cache',
+        ));
     }
 
     public function read(string $environment, Request $request): Response
     {
+        $this->environment = $environment;
         $filters = $request->query->get('filter');
 
         $logSummary    = new LogSummaryGetter(self::PATH, $environment);
@@ -61,11 +71,13 @@ final class LogApiController extends Controller
     {
         $response = new Response();
         if (!isset($filters)) {
-            $response->setContent($logApiBuilder->logSummary());
+            $response->setContent($logApiBuilder->logSummaryFilter($filters));
+            $this->twig->render('logsummary.html.twig', array('environment' => $this->environment, 'logs'=>$logApiBuilder->logSummary()));
         }
 
         if (isset($filters)) {
             $response->setContent($logApiBuilder->logSummaryFilter($filters));
+            $this->twig->render('logsummary.html.twig', array('environment' => $this->environment, 'logs'=>$logApiBuilder->logSummaryFilter($filters)));
         }
 
         return $response;
